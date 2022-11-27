17c091c7c3dddd0eb0c66196df608dced897d811
tpvale <tpv.tiago@gmail.com>
2022-11-26 21:41:28 -0300

    Decima Quinta Aula FEIII

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Login } from "./pages/Login"
 import { SetimaAula } from "./aulas/SetimaAula"
 import { DecimaTerceiraAula } from "./aulas/DecimaTerceiraAula"
 import { DecimaQuartaAula } from "./aulas/DecimaQuartaAula"
+import { DecimaQuintaAula } from "./aulas/DecimaQuintaAula"
 
 function App() {
 
@@ -17,7 +18,8 @@ function App() {
     // <QuintaAula/>
     // <SetimaAula/>
     // <DecimaTerceiraAula/>
-    <DecimaQuartaAula/>
+    // <DecimaQuartaAula/>
+    <DecimaQuintaAula/>
   )
 }
 
